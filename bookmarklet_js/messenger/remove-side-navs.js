45070b03952aa60298f9c543737176d7d8c455d6
T <tyson_ngo@hotmail.com>
2017-09-22 14:19:01 -0400

    added bookmarklet code

---
+++ bookmarklet_js/messenger/remove-side-navs.js
@@ -0,0 +1,1 @@
+javascript:(function(){ var style = document.createElement("style"); var css = document.createTextNode(`._4sp8{min-width:0;} ._1enh, ._4_j5{ display:none; } `); style.appendChild(css); document.getElementsByTagName("head")[0].appendChild(style); })();
