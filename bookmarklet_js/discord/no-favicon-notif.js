83949b694a31fd98d972cb3cc8fce8a6fdc9571a
T <tyson_ngo@hotmail.com>
2018-03-05 00:08:40 -0500

    added bookmarklet js for changing the favicon on discord to default

---
+++ bookmarklet_js/discord/no-favicon-notif.js
@@ -0,0 +1,7 @@
+javascript:(function(){
+    var link = document.createElement("link");
+    link.rel = "icon";
+    link.href= "https://discordapp.com/assets/07dca80a102d4149e9736d4b162cff6f.ico";
+    link.type = "image/png";
+    document.getElementsByTagName("head")[0].appendChild(link);
+})();
