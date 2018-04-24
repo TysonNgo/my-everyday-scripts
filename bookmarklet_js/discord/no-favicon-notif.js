748790507dff05a97972e9b74164870eb5f7f4d4
T <tyson_ngo@hotmail.com>
2018-04-23 23:10:46 -0400

    .

--- bookmarklet_js/discord/no-favicon-notif.js
+++ bookmarklet_js/discord/no-favicon-notif.js
@@ -1,5 +1,7 @@
 javascript:(function(){
+	try{document.getElementById("perma-icon").remove(); return;}catch(err){}
     var link = document.createElement("link");
+    link.id = "perma-icon";
     link.rel = "icon";
     link.href= "https://discordapp.com/assets/07dca80a102d4149e9736d4b162cff6f.ico";
     link.type = "image/png";
