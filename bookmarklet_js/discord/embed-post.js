6a02b9ea2372160f009b2113261cae493cf4a9f2
T <tyson_ngo@hotmail.com>
2018-06-09 20:44:23 -0400

    change bookmarklet embed post discord

--- bookmarklet_js/discord/embed-post.js
+++ bookmarklet_js/discord/embed-post.js
@@ -6,9 +6,22 @@ javascript:(function(){
 	var msg = {
 		embed: {
 			color: parseInt(Math.random().toString(16).substr(2, 6), 16),
-			description: prompt('Message: ')
+			description: prompt('Message: '),
+		    footer: {
+		      icon_url: "https://cdn.discordapp.com/emojis/422822753268400128.gif?v=1",
+		      text: "\u200B"
+		    },
+		    author: {
+		      name: "\u200B",
+		      icon_url: "https://cdn.discordapp.com/emojis/422822753268400128.gif?v=1"
+		    },
+		    thumbnail: {
+		      url: "https://cdn.discordapp.com/emojis/422822753268400128.gif?v=1"
+		    }
 		}
 	};
+
+	if (!msg.embed.description) return;
 
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', `https://discordapp.com/api/v6/channels/${window.location.pathname.split('/').pop()}/messages`);
