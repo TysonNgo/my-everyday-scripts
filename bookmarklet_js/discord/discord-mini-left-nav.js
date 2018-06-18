525b71e75bad6cc598575b5a34ba841739cafb59
Tyson <tyson_ngo@hotmail.com>
2018-06-18 08:30:47 -0400

    update discord left nav bookmarklet to accommodate changes

--- bookmarklet_js/discord/discord-mini-left-nav.js
+++ bookmarklet_js/discord/discord-mini-left-nav.js
@@ -8,62 +8,97 @@ javascript:(function(){
 	/* injected css */
 	var style = document.createElement("style");
 	style.id = "discord_left_nav";
+	/* the container for the guild channels */
 	var channelClass = document.querySelectorAll("div[class^=channel]")[0].classList[0];
 	var css = document.createTextNode(`
-		.${channelClass}, .guilds-wrapper,
-		.avatar-small, .guild, .guilds{ 
-			transition: width 0.5s, height 0.5s, padding 0.5s, background-size 0.5s;
-			transition-timing-function: ease-out;
-		}
-		.guild.unread .guild-inner{
-			background:#8a8e94 !important;
-		}
-		.guild.unread .avatar-small{
-			opacity: 0.5;
-		}
-		.guilds-wrapper .guilds{
-			padding-left: 5px;
-		}
-		.guilds-wrapper:hover .guilds{
-			padding-left: 10px;
-		}
-		.guilds-wrapper .guilds .guild .guild-inner a, .guilds-wrapper .guilds .guild{
-			width: 20px; height: 20px;
-		}
-		.guilds-wrapper:hover .guilds .guild .guild-inner a, .guilds-wrapper:hover .guilds .guild{
-			width: 50px; height: 50px;
-		}
-		.guilds-wrapper .guilds .guild .avatar-small{
-			background-size: 100%;
-		}
-		.guilds-wrapper{
-			width: 30px;
-		}
-		.guilds-wrapper:hover{
-			width: 70px;
-		}
-		.${channelClass}{
-			width: 30px;
-		}
-		.${channelClass}:hover{
-			width: 240px;
-		}
-		.message-group-blocked, .divider > span{
-			display: none; padding: 0;
-		}
-		.message-group{
-			border: 0;
-		}
-		.chat .divider.divider-red > div{
-			background: none;
-		}
-		.chat .new-messages-bar{
-			display:none;
-		}
-		.message-group .image {
-			max-width: 100%;
-			height: auto;
-		}`); style.appendChild(css);
+		/*
+		  shrinking/growing animation for left nav elements
+		  - channel container
+		  - guild container
+		  - guild images
+		*/
+			.${channelClass},
+			.guilds-wrapper,
+			.guild-inner a, .guild-icon, .guild, .guilds{ 
+				transition: width 0.5s, height 0.5s, padding 0.5s, background-size 0.5s;
+				transition-timing-function: ease-out;
+			}
+
+		/* unread guild icon becomes white so it is more visible when the nav is small */
+			.guild.unread .guild-inner{
+				background:#8a8e94 !important;
+			}
+			.guild.unread a{
+				opacity: 0.5;
+			}
+
+		/* change guild container padding */
+			.guilds-wrapper .guilds{
+				padding-left: 5px;
+			}
+			.guilds-wrapper:hover .guilds{
+				padding-left: 10px;
+			}
+
+		/* changes guild icon size */
+			.guilds-wrapper .guilds .guild .guild-inner a,
+			.guilds-wrapper .guilds .guild .guild-inner a .guild-icon,
+			.guilds-wrapper .guilds .guild{
+				width: 20px; height: 20px;
+			}
+			.guilds-wrapper:hover .guilds .guild .guild-inner a,
+			.guilds-wrapper:hover .guilds .guild .guild-inner a .guild-icon,
+			.guilds-wrapper:hover .guilds .guild{
+				width: 50px; height: 50px;
+			}
+
+		/* fits guild icon image */
+			.guilds-wrapper .guilds .guild .guild-icon{
+				background-size: 100%;
+			}
+
+		/* left nav resizers */
+			/* guilds container */
+				.guilds-wrapper{
+					width: 30px;
+				}
+				.guilds-wrapper:hover{
+					width: 70px;
+				}
+			/* channels container */
+				.${channelClass}{
+					width: 30px;
+				}
+				.${channelClass}:hover{
+					width: 240px;
+				}
+
+		/* hides blocked messages */
+			.message-group-blocked, .divider > span{
+				display: none; padding: 0;
+			}
+			.message-group{
+				border: 0;
+			}
+			.chat .divider.divider-red > div{
+				background: none;
+			}
+
+		/* removes N messages unread notif */
+			.chat .new-messages-bar{
+				display:none;
+			}
+
+		/* more responsive image embed */
+			.accessory a[class^=image],
+			.accessory img{
+				max-width: 100%;
+			}
+			.accessory img{
+				height: auto !important;
+			}
+
+		`); style.appendChild(css);
 	document.getElementsByTagName("head")[0].appendChild(style);
 	/* button to toggle style */
 	var toggle = document.createElement("button");
