b773ee38e572cbe4c3624d37b59cecc27e066416
tkn <tyson_ngo@hotmail.com>
2019-03-15 17:44:51 -0400

    removed css rule

--- bookmarklet_js/discord/block-remover.js
+++ bookmarklet_js/discord/block-remover.js
@@ -9,15 +9,11 @@ javascript:(function(){
 			div[class^=messageGroupBlocked], div[class^=divider] > span{
 				display: none; padding: 0;
 			}
-			.message-group{
-				border: 0;
-			}
 			hr[class^=divider],
 			div[class^=divider] > div{
 				display: none !important;
 				background: none !important;
 			}
-
 		`); style.appendChild(css);
 	document.getElementsByTagName("head")[0].appendChild(style);
 
