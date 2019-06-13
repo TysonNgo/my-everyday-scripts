852f0226d2d0e759e4672fd15fb0c196661f1674
T <tyson_ngo@hotmail.com>
2019-06-12 23:47:29 -0400

    scroll thumb hide

---
+++ bookmarklet_js/discord/hide-scroll-thumb.js
@@ -0,0 +1,20 @@
+javascript:(function(){ 
+  if (window.location.host !== 'discordapp.com') return alert('this script is used on "discordapp.com"');
+	try{
+		document.getElementById("discord_scroll_thumb").remove();
+		return;}
+	catch(err){}
+	/* injected css */
+	var style = document.createElement("style");
+	style.id = "discord_scroll_thumb";
+	/* the container for the guild channels */
+	var css = document.createTextNode(`
+		*::-webkit-scrollbar-thumb{
+			display: none;
+		}
+		*::-webkit-scrollbar-thumb:hover{
+			display: inline;
+		}
+		`); style.appendChild(css);
+	document.getElementsByTagName("head")[0].appendChild(style);
+})();
