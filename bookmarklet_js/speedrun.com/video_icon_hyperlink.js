50decd076ef1de44d55c039e4afb5062c4ffd2c8
tkn <tyson_ngo@hotmail.com>
2019-07-14 01:34:10 -0400

    fixed speedrun.com video icon to hyperlink bookmarklet

--- bookmarklet_js/speedrun.com/video_icon_hyperlink.js
+++ bookmarklet_js/speedrun.com/video_icon_hyperlink.js
@@ -1,7 +1,9 @@
 javascript:(function(){
-	document.querySelectorAll('tr[class^=linked]').forEach(tr => {
+	document.querySelectorAll('tr').forEach(tr => {
 		let src = tr.dataset.target;
 		let icon = tr.querySelector('i[class*=fa-video]');
-		icon.outerHTML = `<a href="${src}">${icon.outerHTML}</a>`;
+		if (icon){
+			icon.outerHTML = `<a href="${src}">${icon.outerHTML}</a>`;
+		}
 	})
 })();
