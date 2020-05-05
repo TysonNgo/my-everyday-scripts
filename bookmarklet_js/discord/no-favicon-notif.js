f1b5ee43eca1c9c9a2268826ff79b9d3fadb5203
T <tyson_ngo@hotmail.com>
2020-05-05 18:57:21 -0400

    no favicon

--- bookmarklet_js/discord/no-favicon-notif.js
+++ bookmarklet_js/discord/no-favicon-notif.js
@@ -4,7 +4,7 @@ javascript:(function(){
     var link = document.createElement("link");
     link.id = "perma-icon";
     link.rel = "icon";
-    link.href= "https://discordapp.com/assets/07dca80a102d4149e9736d4b162cff6f.ico";
+    link.href= "https://discord.com/assets/07dca80a102d4149e9736d4b162cff6f.ico";
     link.type = "image/png";
     document.getElementsByTagName("head")[0].appendChild(link);
 })();
