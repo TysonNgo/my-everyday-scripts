d098ce5e0fe00c86d971cf9012d75e277c61d6b3
T <tyson_ngo@hotmail.com>
2022-04-08 19:45:27 -0400

    .

---
+++ bookmarklet_js/twitch/hide_chat_event.js
@@ -0,0 +1,18 @@
+javascript:(function(){
+	const id = 'chat-event-hider';
+	try{
+		document.getElementById(id).remove();
+		return;
+	}
+	catch(err){}
+
+	var style = document.createElement('style');
+	style.id =  id;
+
+	var css = document.createTextNode(`
+		div.InjectLayout-sc-588ddc-0.dizzGc > div > div.Layout-sc-nxg1ff-0{
+			display:none;
+		}`);
+	style.appendChild(css);
+	document.getElementsByTagName("head")[0].appendChild(style);
+})()
